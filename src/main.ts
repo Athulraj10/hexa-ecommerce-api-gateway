4be8f4a0bf1e4fddb6890cc004615834571ed0c8
athulraj9x <athulraj9xtechnologies@gmail.com>
2025-03-29 15:00:12 +0000

    grpc error handle pending

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3011
   await app.listen(PORT);
-  console.log(`API Gateway running on port 3000 ${PORT}`);
+  console.log(`API Gateway running on port ${PORT}`);
 }
 bootstrap();
